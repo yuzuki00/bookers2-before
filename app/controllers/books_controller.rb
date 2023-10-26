de23cd8c76a95e03237c05059d7b971f737a4bca
yuzuki00 <144969224+yuzuki00@users.noreply.github.com>
2023-10-26 03:20:09 +0000

    [Add] 投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,26 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
   end
 
   def index
+    @books = Book.all
   end
 
   def show
   end
+
+  private
+
+  def post_image_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
